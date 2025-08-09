6a92bbbfec6ac85638e6c230e50804bb9109f63d
Regynald Zambrano <regynald.zambrano@gmail.com>
2025-08-09 08:11:55 -0500

    estructura inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+
+<body>
+    <ul>
+        <li><a href="/">Incio</a></li>
+        <li><a href="/">Post</a></li>
+        <li><a href="/">Sobre Mi</a></li>
+        <li><a href="/">Proyecto</a></li>
+    </ul>
+    <h1>Bienvenido a Inicio</h1>
+</body>
+
+</html>
